363f9e33b77d063848876dea61473213ab8c8ccf
Veronika Gabrielová <gabrielova.veronika@gmail.com>
2018-02-27 21:21:29 +0100

    Work on boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export type AppProps = {
+  };
+ 
+
+export default class App extends React.Component<{}, {}> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { //
+    };
+  }
+
+  render() {
+    return (
+        <div/>
+      );
+    }
+  }
